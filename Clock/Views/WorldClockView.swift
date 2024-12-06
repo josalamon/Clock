3bbda17a7c1979eedbfdb00c9dd6473ff9d5d545
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-06 14:53:18 -0500

    added tool bar items to the interface.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct WorldClockView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("World Clock")
+                .navigationTitle("World Clock")
+                .toolbar {
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        
+                        Button("Edit") {
+                            // Does nothing right now
+                        }
+                        
+                    }
+                    
+                    ToolbarItem(placement: .primaryAction) {
+                        
+                        Button {
+                            //Does nothing right now
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        
+                    }
+                    
+                }
+        }
     }
 }
-
 #Preview {
-    WorldClockView()
+    LandingView()
 }

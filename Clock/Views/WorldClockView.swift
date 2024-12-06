139e65f0da8d9b95e3a0e3d59593aae18147cb22
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-06 15:27:14 -0500

    ...

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -12,11 +12,14 @@ struct WorldClockView: View {
         NavigationStack {
             VStack{
                 // Ottawa
+                ExtractedView(timeZoneOffset: "+0", city: "Ottawa", time: "6:35", amOrPm: "AM")
+                
+                // Vancouver
                 HStack{
                     //Left side
                     VStack{
-                        Text("Today, +0HRS")
-                        Text("Ottawa")
+                        Text("Today, -3HRS")
+                        Text("Vancouver")
                             .font(.system(.largeTitle, design: .default,weight: .thin))
                     }
                     
@@ -60,3 +63,35 @@ struct WorldClockView: View {
 #Preview {
     LandingView()
 }
+
+struct ExtractedView: View {
+    
+    // MARK: Stored Properties
+    let timeZoneOffset: String
+    let city: String
+    let time: String
+    let amOrPm: String
+    
+    //MARK: Computed Properties
+    var body: some View {
+        HStack{
+            //Left side
+            VStack{
+                Text("Today, +0HRS")
+                Text("Ottawa")
+                    .font(.system(.largeTitle, design: .default,weight: .thin))
+            }
+            
+            Spacer()
+            
+            // Right side
+            Text("6:35")
+                .font(.system(size: 64.0, weight: .thin, design: .default))
+            Text("AM")
+                .font(.system(.largeTitle,design: .default,weight: .thin))
+            
+        }
+    }
+}
+
+

b6d82a31b28fba2e5cf4ebd4aaa7bd8f60566cf9
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-06 14:35:23 -0500

    replaced VStack with other code and made more views

---
+++ Clock/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-06.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorldClockView()
+}

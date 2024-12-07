b8ef7009da9f8f7ab5a87415c439ab8e64dfb5db
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-07 10:03:37 -0500

    Applied abstraction to show many cities in the world clock user interface using a helper view.

---
+++ Clock/Views/CityView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct CityView: View {
+    
+    // MARK: Stored Properties
+    let timeZoneOffset: String
+    let city: String
+    let time: String
+    let amOrPm: String
+    
+    //MARK: Computed Properties
+    var body: some View {
+        HStack{
+            //Left side
+            VStack{
+                Text("Today, \(timeZoneOffset)HRS")
+                Text(city)
+                    .font(.system(.largeTitle, design: .default,weight: .thin))
+            }
+            
+            Spacer()
+            
+            // Right side
+            Text(time)
+                .font(.system(size: 64.0, weight: .thin, design: .default))
+            Text(amOrPm)
+                .font(.system(.largeTitle,design: .default,weight: .thin))
+            
+        }
+
+    }
+}
